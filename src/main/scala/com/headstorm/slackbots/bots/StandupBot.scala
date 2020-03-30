bb6feb4cb5ea9ad227dae723d7c6dc3a747cab15
nmerillH <nmerrill@headstorm.com>
2020-03-30 16:08:15 -0500

    commented to reduce to standup bot only and added logger to see messaging format

--- src/main/scala/com/headstorm/slackbots/bots/StandupBot.scala
+++ src/main/scala/com/headstorm/slackbots/bots/StandupBot.scala
@@ -1,6 +1,7 @@
 package com.headstorm.slackbots.bots
 
 import com.headstorm.slackbots.config._
+import scribe._
 
 /**
   * Async standup Bot
@@ -10,15 +11,17 @@ class StandupBot[F[_]] extends Bot {
   lazy val token: Token = config.slackbots.standup.token
   lazy val channel: String = config.slackbots.standup.channelId
 
-  client.onMessage { message =>
+  client.onMessage { message  =>
+    this.logger.info(s"Message received $message")
     val statusTitle: Iterator[String] = Iterator.apply("Feeling", "Yesterday", "Today", "Blocked")
     val userName: String = state.getUserById(message.user).map(_.name).getOrElse("unknown user")
+    this.logger.info(s"Channel is $channel")
     val sendToChannel: String = message.text
       .split("\\|")
       .toList
       .map(i => s"*${statusTitle.next}*\n>$i")
       .mkString(s"*Standup for: ${userName}*\n\n", "\n", "")
-
+    
     client.sendMessage(channel, sendToChannel.mkString)
   }
 }
