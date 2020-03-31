bacd42162834e335f7be1d544d4c39679b14a955
nmerillH <nmerrill@headstorm.com>
2020-03-31 15:05:06 -0500

    fixed echo in channel

--- src/main/scala/com/headstorm/slackbots/bots/StandupBot.scala
+++ src/main/scala/com/headstorm/slackbots/bots/StandupBot.scala
@@ -10,18 +10,19 @@ class StandupBot[F[_]] extends Bot {
 
   lazy val token: Token = config.slackbots.standup.token
   lazy val channel: String = config.slackbots.standup.channelId
+  if (channel.equalsIgnoreCase("D010EU2MKEY")) {
+    client.onMessage { message =>
+      this.logger.info(s"Message received $message")
+      val statusTitle: Iterator[String] = Iterator.apply("Feeling", "Yesterday", "Today", "Blocked")
+      val userName: String = state.getUserById(message.user).map(_.name).getOrElse("unknown user")
+      this.logger.info(s"Channel is $channel")
+      val sendToChannel: String = message.text
+        .split("\\|")
+        .toList
+        .map(i => s"*${statusTitle.next}*\n>$i")
+        .mkString(s"*Standup for: ${userName}*\n\n", "\n", "")
 
-  client.onMessage { message  =>
-    this.logger.info(s"Message received $message")
-    val statusTitle: Iterator[String] = Iterator.apply("Feeling", "Yesterday", "Today", "Blocked")
-    val userName: String = state.getUserById(message.user).map(_.name).getOrElse("unknown user")
-    this.logger.info(s"Channel is $channel")
-    val sendToChannel: String = message.text
-      .split("\\|")
-      .toList
-      .map(i => s"*${statusTitle.next}*\n>$i")
-      .mkString(s"*Standup for: ${userName}*\n\n", "\n", "")
-    
-    client.sendMessage(channel, sendToChannel.mkString)
+      client.sendMessage(channel, sendToChannel.mkString)
+    }
   }
 }
